9313220cb559c4cb863b677e4c650723f8538a82
DStefanskaPolek <122095949+DStefanskaPolek@users.noreply.github.com>
2023-09-19 16:58:51 +0200

    Update Fibonacci sequence recursion.py

--- Fibonacci sequence/Fibonacci sequence recursion.py
+++ Fibonacci sequence/Fibonacci sequence recursion.py
@@ -1,10 +1,10 @@
-# cache-dictionary declaration,
+# declares cache-dictionary,
 # as we know the 0th and 1st Fibonacci's number
 # this is also the base case of recursion
 sequence = {0: 0, 1: 1}
 
 
-# the function first checks whether the needed number is in the dictionary,
+# function first checks whether the needed number is in the dictionary,
 # if so, it retrieves it from the dictionary,
 # if not, it calculates it recursively and then saves it to the dictionary for reuse
 def recursion_Fibonacci(n):
@@ -14,20 +14,19 @@ def recursion_Fibonacci(n):
     return sequence[n]
 
 
-# getting the value from the user and converting the type from string to int
+# gets value from the user and converts the type from string to int
 numbers = int(input("How many Fibonacci numbers you want to display? "))
 
-# calling a function for a value provided by the user
+# calls the function for the value provided by the user
 recursion_Fibonacci(numbers)
 
-# displaying the content of the dictionary up to the value provided by the user
-# when the value is 0, not the entire dictionary is displayed, but only the 0th number
-# (yea, fixing my bugs)
+# displays the content of the dictionary up to the value provided by the user
+# when the value is 0, the entire dictionary is not displayed, but only the 0th number
 for i in range(numbers + 1):
     print(sequence[i], end=" ")
 
-# calculating the golden ratio value
-# and handling an exception if the sequence is too short
+# calculates the golden ratio value
+# and handles an exception if the sequence is too short
 try:
     print("\n\nThe golden ratio of the two largest numbers in the range is", sequence[numbers] / sequence[numbers - 1])
 except (ZeroDivisionError, KeyError):
